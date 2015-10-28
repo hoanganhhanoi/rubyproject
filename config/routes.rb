c382d67fe5e7b70cc37b6fd844d9474501d8a0ea
hoanganh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-10-28 16:01:14 +0700

    config layout web, use Bootstrap into Web Sample and change path of page use to get 'name page' => 'path'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   resources :microposts
   resources :users
