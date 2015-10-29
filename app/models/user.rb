16d1bb66f9f90d7ff96031248e26827f53376783
hoanganh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-10-29 11:44:09 +0700

    complete session for user login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, if: -> { password.present? }
   validates :password_confirmation, presence: true
 
+  # Returns the hash digest of the given string
+  def User.digest(string)
+
+    cost = ActiveModel::SecurePassword.min_cost? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+    
+  end
+
 end
